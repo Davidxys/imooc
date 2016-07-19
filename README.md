b9fdb683169f7f36847cf3ec1fba12be89c9e129
Davidxys <xuyongsh@outlook.com>
2016-07-19 08:17:57 +0800

    update readme: add link how to use jade

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 1. npm install express jade moment mongoose
 2. npm install -g bower
 3. bower install bootstrap
+
+how to use jade: http://www.w3cplus.com/html/how-to-use-jade.html
--- imooc/app.js
+++ imooc/app.js
@@ -14,85 +14,92 @@ app.listen(port)
 console.log('immoc started on port ' + port)
 //index page
 app.get('/',function(req, res){
-	res.render('index',{
-		title: 'imooc 首页',
-		movies:[{
-			title: '机械战警',
-			_id:1,
-			poster:'http://r3.ykimg.com/0516000530EEB63675839160D0B79D5'
-		},{
-			title: '机械战警',
-			_id:2,
-			poster:'http://v.youku.com/v_show/id_XMTU3NTkxNDIwMA==.html'
-		},{
-			title: '机械战警',
-			_id:3,
-			poster:'http://v.youku.com/v_show/id_XMTU3NTkxNDIwMA==.html'
-		},{
-			title: '机械战警',
-			_id:4,
-			poster:'http://v.youku.com/v_show/id_XMTU3NTkxNDIwMA==.html'
-		},{
-			title: '机械战警',
-			_id:5,
-			poster:'http://v.youku.com/v_show/id_XMTU3NTkxNDIwMA==.html'
-		},{
-			title: '机械战警',
-			_id:6,
-			poster:'http://v.youku.com/v_show/id_XMTU3NTkxNDIwMA==.html'
-		}
-		]
-	})
+    res.render('index',{
+        title: 'imooc 首页',
+        movies:[{
+            title: '机械战警',
+            _id:1,
+            poster:'http://r3.ykimg.com/0516000530EEB63675839160D0B79D5'
+        },{
+            title: '机械战警',
+            _id:2,
+            poster:'http://v.youku.com/v_show/id_XMTU3NTkxNDIwMA==.html'
+        },{
+            title: '机械战警',
+            _id:3,
+            poster:'http://v.youku.com/v_show/id_XMTU3NTkxNDIwMA==.html'
+        },{
+            title: '机械战警',
+            _id:4,
+            poster:'http://v.youku.com/v_show/id_XMTU3NTkxNDIwMA==.html'
+        },{
+            title: '机械战警',
+            _id:5,
+            poster:'http://v.youku.com/v_show/id_XMTU3NTkxNDIwMA==.html'
+        },{
+            title: '机械战警',
+            _id:6,
+            poster:'http://v.youku.com/v_show/id_XMTU3NTkxNDIwMA==.html'
+        }
+        ]
+    })
 })
 
 //detail page
 app.get('/detail',function(req, res){
-	res.render('detail',{
-		title: 'imooc 详情',
-		movie:{
-			doctor:'何塞.趴地利亚',
-			country: 'US',
-			title:'机械战警',
-			year: 2014,
-			poster:'http://v.youku.com/v_show/id_XMTU3NTkxNDIwMA==.html',
-			language:'En',
-			poster:'http://v.youku.com/v_show/id_XMTU3NTkxNDIwMA==.html',
-			summary: 'dddkk dfkjdkfjdkjl dfdfd'
-		}
-	})
+    res.render('detail',{
+        title: 'imooc 详情',
+        movie:{
+            doctor:'何塞.趴地利亚',
+            country: 'US',
+            title:'机械战警',
+            year: 2014,
+            poster:'http://v.youku.com/v_show/id_XMTU3NTkxNDIwMA==.html',
+            language:'En',
+            poster:'http://v.youku.com/v_show/id_XMTU3NTkxNDIwMA==.html',
+            summary: 'dddkk dfkjdkfjdkjl dfdfd'
+        }
+    })
 })
 
 //admin page
 app.get('/admin',function(req, res){
-	res.render('admin',{
-		title: 'imooc 后台录入页',
-		movie:{
-			title:'',
-			doctor:'',
-			country:'',
-			year:'',
-			poster:'',
-			flash:'',
-			summary:'',
-			language:''
-		}
-	})
+    res.render('admin',{
+        title: 'imooc 后台录入页',
+        movie:{
+            title:'',
+            doctor:'',
+            country:'',
+            year:'',
+            poster:'',
+            flash:'',
+            summary:'',
+            language:''
+        }
+    })
 })
 
 //list page
 app.get('/list',function(req, res){
-	res.render('list',{
-		title: 'imooc 列表页',
-		movies:[{
-			title:'机械战警',
-			_id:1,
-			doctor:'何塞.趴地利亚',
-			country: 'US',
-			year: 2014,
-			poster:'http://v.youku.com/v_show/id_XMTU3NTkxNDIwMA==.html',
-			language:'En',
-			poster:'http://v.youku.com/v_show/id_XMTU3NTkxNDIwMA==.html',
-			summary: 'dddkk dfkjdkfjdkjl dfdfd'
-		}]
-	})
+    res.render('list',{
+        title: 'imooc 列表页',
+        movies:[{
+            title:'机械战警',
+            _id:1,
+            doctor:'何塞.趴地利亚',
+            country: 'US',
+            year: 2014,
+            poster:'http://v.youku.com/v_show/id_XMTU3NTkxNDIwMA==.html',
+            language:'En',
+            poster:'http://v.youku.com/v_show/id_XMTU3NTkxNDIwMA==.html',
+            summary: 'dddkk dfkjdkfjdkjl dfdfd'
+        }]
+    })
+})
+//test page
+app.get('/test', function(req, res) {
+    res.render('test', {
+        title:'test page',
+        books:['abc','def','czz']
+    })
 })
